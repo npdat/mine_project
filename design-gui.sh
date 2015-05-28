40f96f7821211e7a324aa0d303a77ce959b95eb9
Dat Ngo <npdat@tma.com.vn>
2015-05-28 08:19:10 +0700

    update ruby 2.2.2

--- design-gui.sh
+++ design-gui.sh
@@ -1,2 +1,4 @@
 mv app/views/layouts/application.html.erb app/views/layouts/application.html.haml
 mv app/assets/stylesheets/application.css app/assets/stylesheets/application.css.scss
+
+RAILS_ENV=production bundle exec rake assets:precompile
